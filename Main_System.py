188c18f4cb44bc27b5de44aa50d2ca1766caa240
Sepehr Tabrizchi <stabrizchi2@unl.edu>
2024-01-10 15:35:10 -0600

    pixel array and adc delay are added

--- Main_System.py
+++ Main_System.py
@@ -21,14 +21,24 @@ class Main_System(Component.Component):
         #Create the network
         self._network = Network.Network(config)
 
-
+        #parameters: 
+        self.parallelism_level = int(config["HardwareConfig"]["parallelism_level"])
         #power:
         self.global_memory_write_power = self.total_write_in_global()
         self.global_memory_read_power = self.total_read_in_global()
         self.buffer_memory_write_power = self.total_write_in_buffer()
         self.buffer_memory_read_power = self.total_read_in_buffer()
 
+        #delay
+        self.sensing_pixel_array_delay = self._pixel_array.total_delay_in_sensing
+        self.sensing_adc_array_delay = self._adc_array.total_delay_in_sensing * self._pixel_array.height
 
+        self.compute_pixel_array_delay = self._pixel_array.total_delay_in_compute
+        self.compute_adc_array_delay = (self._adc_array.total_delay_in_compute *  self._network.calculate_output_height(self._pixel_array.height)  * self._network.kernel_number) / self.parallelism_level
+        #TODO: memory delays
+        self.sensing_delay = self.total_sensing_delay()
+        
+        self.computing_delay = self._pixel_array.total_delay_in_compute + (self._adc_array.total_delay_in_compute *  self._network.calculate_output_height(self._pixel_array.height)) 
         
     def total_write_in_global(self):
         return self._global_memory.write_power_per_weight * self._network.total_weights
@@ -41,6 +51,12 @@ class Main_System(Component.Component):
     
     def total_read_in_buffer(self):
         return self._network.calculate_output_height(self._pixel_array.height) * self._buffer_memory.read_power_per_weight * self._network.total_weights
+
+    def total_sensing_delay(self):
+        return self.sensing_pixel_array_delay + self.sensing_adc_array_delay
+
+    def total_compute_delay(self):
+        pass
 
     def print_detail(self, tab = ""):
         result = ""
